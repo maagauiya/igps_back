a9bf072463a773644dff1078112a07608522c637
Bekarys Magauiya <maagauiya@MacBook-Air-Bekarys.local>
2021-11-30 13:23:19 +0600

    js

--- testsite/app1/views.py
+++ testsite/app1/views.py
@@ -1,12 +1,16 @@
+import json
+from django.core import serializers
 from django.http.response import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
-
+import json 
+from django.forms.models import model_to_dict
 
 from .models import*
+
 # Create your views here.
 
 def index(request):
@@ -21,15 +25,21 @@ def index(request):
             return HttpResponse('unsucc')
     else:
         return render(request,'app1/signin.html')
-        
+
 @login_required(login_url='')
 def checker(request,user,assetid):
-    devices=App1.objects.filter(user=assetid)
-    context={
-        'animals':devices,
-    }
+
+
+    # devices=App1.objects.filter(user=assetid)
+
+    # context={
+    #     'animals':devices,
+    # }
+
+    devices=serializers.serialize("json",App1.objects.filter(user=assetid))
+
     if user is not None:
-        return render(request,'app1/test.html',context=context)
+        return render(request,'app1/map.html', devices)
     else:
         return HttpResponse('unsucc')
 
